94181d7d4148dbb26dc2e0816365d1613591da52
danimdev <106762674+danimdev@users.noreply.github.com>
2022-08-05 15:15:54 +0200

    Added A Menu And Updated the Database Migration

---
+++ Migrations/20220805125758_newMigration2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserDatabaseConsoleApp.Migrations
+{
+    public partial class newMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,79 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using UserDatabaseConsoleApp;
 
 MainOutput();
 
+
 static void MainOutput()
 {
+    while (true)
+    {
+        Console.WriteLine("1. Show Database Table");
+        Console.WriteLine("2. Add User to Database Table");
+        Console.WriteLine("3. Exit Program");
+        int num = Convert.ToInt32(Console.ReadLine());
 
+        switch (num)
+        {
+            case 1:
+                ShowUserData();
+                break;
+            case 2:
+                AddDataToDatabase();
+                break;
+            case 3:
+                return;
+                break;
+            default:
+                Console.WriteLine("Non Avaible Option...");
+                break;
+        }
+    }
+}
+
+static void ShowUserData()
+{
+    using (var db = new UserDB())
+    {
+        Console.WriteLine("Data Retrieved From Database:");
+        foreach (var user in db.Users)
+        {
+            Console.WriteLine("ID: " + user.ID + " Username: " + user.Username + " Password: " + user.Password);
+        }
+    }
+}
+
+static void AddDataToDatabase()
+{
+    string username, password;
+
+    while (true)
+    {
+        Console.Write("Username: ");
+        username = Console.ReadLine();
+        Console.Write("Password: ");
+        password = Console.ReadLine();
+
+        if (!String.IsNullOrEmpty(username) && !String.IsNullOrEmpty(password))
+        {
+            using (var db = new UserDB())
+            {
+                Console.WriteLine("Adding data to Database...");
+                var user = new User();
+                user.Username = username;
+                user.Password = password;
+
+                db.Add(user);
+
+                db.SaveChanges();
+            }
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Username or Password cannot be empty...");
+        }
+    }
+
+    Console.WriteLine("Data Was Added to Database...");
 }
